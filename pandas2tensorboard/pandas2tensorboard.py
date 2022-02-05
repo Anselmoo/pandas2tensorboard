02eee3915e68b2ba815c7733eb18f015f87a28b7
Anselm Hahn <Anselm.Hahn@gmail.com>
2022-02-05 22:57:57 +0100

    Intial commit

---
+++ pandas2tensorboard/pandas2tensorboard.py
@@ -0,0 +1,191 @@
+try:
+    import modin.pandas as pd
+
+except ImportError:
+    import pandas as pd
+
+from pathlib import Path
+from typing import MutableMapping, Tuple
+
+import numpy as np
+from pandas import DataFrame
+from torch.utils.tensorboard import SummaryWriter
+
+
+class Pandas2Tensorboard:
+    def __init__(
+        self,
+        log_dir: MutableMapping[Path, str] = None,
+        comment: str = "",
+        purge_step: int = None,
+        max_queue: int = 10,
+        flush_secs: int = 120,
+        filename_suffix: str = "",
+    ) -> None:
+        self.log_dir = log_dir
+        self.comment = comment
+        self.purge_step = purge_step
+        self.max_queue = max_queue
+        self.flush_secs = flush_secs
+        self.filename_suffix = filename_suffix
+        self.initialize()
+
+    def initialize(self) -> None:
+        self.writer = SummaryWriter(
+            log_dir=self.log_dir,
+            comment=self.comment,
+            purge_step=self.purge_step,
+            max_queue=self.max_queue,
+            flush_secs=self.flush_secs,
+            filename_suffix=self.filename_suffix,
+        )
+
+    def regular_df(
+        self,
+        df: pd.DataFrame,
+        group: str = None,
+        label: str = "",
+        remove_nan: bool = False,
+        remove_str: bool = False,
+    ) -> None:
+        """Convert a pandas dataframe to a tensorboard timeseries.
+
+        Args:
+            df (pd.DataFrame): Dataframe to convert.
+            group (str, optional): Group name for the labels. Defaults to None.
+            label (str, optional): Name of the label. Defaults to "".
+            remove_nan (bool, optional): Remove rows with `NaNs` . Defaults to False.
+            remove_str (bool, optional): Remove columns with `str`. Defaults to False.
+        """
+
+        df = self.df_cleaning(df=df, remove_nan=remove_nan, remove_str=remove_str)
+        tag = self.create_tag(group=group, label=label)
+
+        for i, val in enumerate(df.to_dict(orient="records")):
+            self.writer.add_scalars(main_tag=tag, tag_scalar_dict=val, global_step=i)
+
+    def timeseries_df(
+        self,
+        df: pd.DataFrame,
+        time: str,
+        time_convert: bool = True,
+        group: str = None,
+        label: str = "",
+        remove_nan: bool = False,
+        remove_str: bool = False,
+    ) -> None:
+        """Convert a pandas dataframe to a tensorboard timeseries.
+
+        Args:
+            df (pd.DataFrame): Dataframe to convert.
+            time (str): Column name of the time column.
+            time_convert (bool, optional): Convert str `timestamp` to `float`. Defaults to True.
+            group (str, optional): Group name for the labels. Defaults to None.
+            label (str, optional): Name of the label. Defaults to "".
+            remove_nan (bool, optional): Remove rows with `NaNs` . Defaults to False.
+            remove_str (bool, optional): Remove columns with `str`. Defaults to False.
+        """
+        if time_convert:
+            df[time] = pd.to_datetime(df[time]).apply(lambda x: x.timestamp())
+        df = self.df_cleaning(df=df, remove_nan=remove_nan, remove_str=remove_str)
+        tag = self.create_tag(group=group, label=label)
+        df_x, df_y = self.split_df(df=df, col=time)
+        for i, val_time in enumerate(zip(df_x.values, df_y.to_dict(orient="records"))):
+            self.writer.add_scalars(
+                main_tag=tag,
+                tag_scalar_dict=val_time[1],
+                global_step=i,
+                walltime=val_time[0],
+            )
+
+    def scatter_df(
+        self,
+        df: pd.DataFrame,
+        x_axis: str,
+        group: str = None,
+        remove_nan: bool = False,
+        remove_str: bool = False,
+    ) -> None:
+        """[summary]
+
+        Args:
+            df (pd.DataFrame): [description]
+            x_axis (str): [description]
+            group (str, optional): Group name for the labels. Defaults to None.
+            remove_nan (bool, optional): Remove rows with `NaNs` . Defaults to False.
+            remove_str (bool, optional): Remove columns with `str`. Defaults to False.
+        """
+        df = self.df_cleaning(df=df, remove_nan=remove_nan, remove_str=remove_str)
+        tag = self.create_tag(group=group, label=x_axis)
+        df_x, df_y = self.split_df(df=df, col=x_axis)
+
+        for x_dict, y_dict in zip(df_x.values, df_y.to_dict(orient="records")):
+            self.writer.add_hparams({tag: x_dict}, y_dict)
+
+    def close(self) -> None:
+        self.writer.close()
+
+    @staticmethod
+    def split_df(df: pd.DataFrame, col: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """[summary]
+
+        Args:
+            df (pd.DataFrame): [description]
+            col (str): [description]
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: [description]
+        """
+        return df[col], df.drop(columns=[col])
+
+    @staticmethod
+    def df_cleaning(
+        df: pd.DataFrame, remove_nan: bool, remove_str: bool
+    ) -> pd.DataFrame:
+        """Remove nan and objects from DataFrame
+
+        Args:
+            df (pd.DataFrame): [description]
+            remove_nan (bool): [description].
+            remove_str (bool): [description].
+
+        Returns:
+            pd.DataFrame: [description]
+        """
+        _df = df.copy()
+        if remove_nan:
+            _df = _df.dropna(axis=0, how="any")
+        if remove_str:
+            _df = _df.select_dtypes(include=["number", "datetime64[ns]"])
+        return _df
+
+    @staticmethod
+    def create_tag(label: str, group: str = None) -> str:
+        """Create tag for tensorboard
+
+        Args:
+            label (str): Label name
+            group (str, optional): Group name. Defaults to None.
+
+        Returns:
+            str: Tag name as concatenation of group (optional) and label
+        """
+        return f"{group}/{label}" if group else label
+
+
+if __name__ == "__main__":
+    # import numpy as np
+
+    import seaborn as sns
+
+    df = sns.load_dataset("taxis")
+    pt = Pandas2Tensorboard()
+    pt.timeseries_df(
+        sns.load_dataset("taxis"),
+        time="pickup",
+        label="taxis",
+        remove_nan=True,
+        remove_str=True,
+    )
+    # pt.regular_df(sns.load_dataset("penguins"), group="penguins2", label="penguins2")
+    pt.close()
---
+++ pandas2tensorboard/test/test_transformer.py
@@ -0,0 +1,56 @@
+import seaborn as sns
+
+from pandas2tensorboard import pandas2tensorboard as p2t
+
+
+def test_scatter():
+    pt = p2t.Pandas2Tensorboard()
+    pt.scatter_df(
+        sns.load_dataset("anagrams"),
+        x_axis="subidr",
+        group="anagrams",
+        remove_nan=True,
+        remove_str=True,
+    )
+    pt.close()
+    assert True
+
+
+class TestRemove:
+    def test_remove_false(self):
+        pt = p2t.Pandas2Tensorboard()
+        pt.regular_df(
+            sns.load_dataset("gammas").drop(columns=["ROI"]),
+            label="subidr",
+            remove_nan=False,
+            remove_str=False,
+        )
+        pt.close()
+        assert True
+
+    def test_remove_true(self):
+        pt = p2t.Pandas2Tensorboard()
+        pt.regular_df(
+            sns.load_dataset("planets"),
+            label="planets",
+            remove_nan=True,
+            remove_str=True,
+        )
+        pt.close()
+        assert True
+
+
+class TestLabelGroup:
+    def test_group(self):
+        pt = p2t.Pandas2Tensorboard()
+        pt.regular_df(
+            sns.load_dataset("anagrams"), group="group", label="label", remove_str=True
+        )
+        pt.close()
+        assert True
+
+    def test_only_label(self):
+        pt = p2t.Pandas2Tensorboard()
+        pt.regular_df(sns.load_dataset("anagrams"), label="label_only", remove_str=True)
+        pt.close()
+        assert True
